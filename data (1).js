5aa401f112d061fde501d85f4aaaa81254220c82
Foxtie9 <157839364+Foxtie9@users.noreply.github.com>
2025-01-17 14:13:44 +0530

    Updata (1).js

--- data (1).js
+++ data (1).js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "6384853813:AAEqmayHJ2eR6croLISJGUsWaOmim6uD4Dw", // Replace with your bot's token
+  BOT_TOKEN: "add your bot token", // Replace with your bot's token
   SERVER_URL: "https://your-repl-url",  // Replace with your Replit server URL
   CHANNELS: [
     { name: "Thoxer-Hack💀", link: "https://t.me/Thoxer_hack" },
